678e2ab87b09cf11d5393ecfa0f67924c60dcacc
Ning <547568644@qq.com>
2015-04-08 18:44:51 +0800

    。

--- src/main/java/ui/HotPanel.java
+++ src/main/java/ui/HotPanel.java
@@ -26,7 +26,7 @@ public class HotPanel extends JPanel {
 		setTag();
 		setShow();
 		this.add(tag);
-		this.add(team);
+		this.add(show);
 		this.repaint();
 		
 	} 
@@ -34,18 +34,19 @@ public class HotPanel extends JPanel {
 	void setTag() {
 
 		tag.setLayout(null);
-		tag.setBounds(0, 0, FrameSize.width / 5,
+		tag.setBounds(0, 0, FrameSize.width / 6,
 				11 * FrameSize.height / 12);
+		int height=11 * FrameSize.height / 12;
 		tag.setBackground(FrameSize.backColor);
 		JButton today_player = new JButton("当天热点球员");
 		JButton season_player = new JButton("赛季热点球员");
 		JButton season_team = new JButton("赛季热点球队");
 		JButton fast_player = new JButton("进步最快球员");
 
-		today_player.setBounds(10, FrameSize.height / 12 , 150, 50);
-		season_player.setBounds(10, FrameSize.height / 12 + 100, 150, 50);
-		season_team.setBounds(10, FrameSize.height / 12 + 200, 150, 50);
-		fast_player.setBounds(10, FrameSize.height / 12 + 300, 150, 50);
+		today_player.setBounds(10, 40 , 150, 50);
+		season_player.setBounds(10, 40+height /4, 150, 50);
+		season_team.setBounds(10, 40+height / 2, 150, 50);
+		fast_player.setBounds(10, 40+3*height/4, 150, 50);
 		
 		today_player.setBackground(FrameSize.buttonbackColor);
 		season_player.setBackground(FrameSize.buttonbackColor);
@@ -67,18 +68,18 @@ public class HotPanel extends JPanel {
 	void setShow(){
 		show.setLayout(null);
 		show.setBackground(FrameSize.backColor);
-		show.setBounds(FrameSize.width / 5,0, 4*FrameSize.width / 5,
+		show.setBounds(FrameSize.width / 6,0, 5*FrameSize.width / 6,
 				11 * FrameSize.height / 12);
 		
 		JComboBox<String> choose;
 		choose = new MyComboBox(new String[] { "得分","篮板","助攻","抢断","盖帽" });
-		choose.setBounds(10, 5, 80, 30);
+		choose.setBounds(10, 50, 80, 30);
 		
 		JLabel portrait_1=new JLabel("头像");
 //		portraitLabel.setIcon(scaleImage(new ImageIcon(),115,92));
 		portrait_1.setBackground(FrameSize.buttonbackColor);
 		portrait_1.setOpaque(true);
-		portrait_1.setBounds(FrameSize.width/12, 80,185 ,123);
+		portrait_1.setBounds(FrameSize.width/3-228, 80,185 ,123);
 		
 		JLabel portrait_2=new JLabel("头像");
 //		portraitLabel.setIcon(scaleImage(new ImageIcon(),115,92));
@@ -105,23 +106,41 @@ public class HotPanel extends JPanel {
 		portrait_5.setBounds(FrameSize.width/3-100, 480,57 ,46);
 
 		
-		UneditableTextField num_1=new UneditableTextField();
-		UneditableTextField num_2=new UneditableTextField();
-		UneditableTextField num_3=new UneditableTextField();
-		UneditableTextField num_4=new UneditableTextField();
-		UneditableTextField num_5=new UneditableTextField();
+		UneditableTextField name_1=new UneditableTextField();
+		UneditableTextField name_2=new UneditableTextField();
+		UneditableTextField name_3=new UneditableTextField();
+		UneditableTextField name_4=new UneditableTextField();
+		UneditableTextField name_5=new UneditableTextField();
 		
-		num_1.setBounds(FrameSize.width/3, 80, 200, 80);
-		num_2.setBounds(FrameSize.width/3, 240, 200, 50);
-		num_3.setBounds(FrameSize.width/3, 320, 200, 50);
-		num_4.setBounds(FrameSize.width/3, 400, 200, 50);
-		num_5.setBounds(FrameSize.width/3, 480, 200, 50);
+		name_1.setBounds(FrameSize.width/3, 80, 200, 80);
+		name_2.setBounds(FrameSize.width/3, 240, 200, 50);
+		name_3.setBounds(FrameSize.width/3, 320, 200, 50);
+		name_4.setBounds(FrameSize.width/3, 400, 200, 50);
+		name_5.setBounds(FrameSize.width/3, 480, 200, 50);
 		
-		num_1.setBackground(FrameSize.buttonbackColor);
-		num_2.setBackground(FrameSize.buttonbackColor);
-		num_3.setBackground(FrameSize.buttonbackColor);
-		num_4.setBackground(FrameSize.buttonbackColor);
-		num_5.setBackground(FrameSize.buttonbackColor);
+		name_1.setBackground(FrameSize.buttonbackColor);
+		name_2.setBackground(FrameSize.buttonbackColor);
+		name_3.setBackground(FrameSize.buttonbackColor);
+		name_4.setBackground(FrameSize.buttonbackColor);
+		name_5.setBackground(FrameSize.buttonbackColor);
+		
+		UneditableTextField score_1=new UneditableTextField();
+		UneditableTextField score_2=new UneditableTextField();
+		UneditableTextField score_3=new UneditableTextField();
+		UneditableTextField score_4=new UneditableTextField();
+		UneditableTextField score_5=new UneditableTextField();
+		
+		score_1.setBounds(FrameSize.width/3+250, 80, 100, 100);
+		score_2.setBounds(FrameSize.width/3+250, FrameSize.height / 7+100, 80, 80);
+		score_3.setBounds(FrameSize.width/3+250, 2*FrameSize.height / 7+100, 80, 80);
+		score_4.setBounds(FrameSize.width/3+250, 3*FrameSize.height / 7+100, 80, 80);
+		score_5.setBounds(FrameSize.width/3+250, 4*FrameSize.height / 7+100, 80, 80);
+		
+		score_1.setBackground(FrameSize.buttonbackColor);
+		score_2.setBackground(FrameSize.buttonbackColor);
+		score_3.setBackground(FrameSize.buttonbackColor);
+		score_4.setBackground(FrameSize.buttonbackColor);
+		score_5.setBackground(FrameSize.buttonbackColor);
 		
 		show.add(choose);
 		show.add(portrait_1);
@@ -129,11 +148,16 @@ public class HotPanel extends JPanel {
 		show.add(portrait_3);
 		show.add(portrait_4);
 		show.add(portrait_5);
-		show.add(num_1);
-		show.add(num_2);
-		show.add(num_3);
-		show.add(num_4);
-		show.add(num_5);
+		show.add(name_1);
+		show.add(name_2);
+		show.add(name_3);
+		show.add(name_4);
+		show.add(name_5);
+		show.add(score_1);
+		show.add(score_2);
+		show.add(score_3);
+		show.add(score_4);
+		show.add(score_5);
 	
 	}
 
