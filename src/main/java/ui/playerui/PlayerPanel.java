130f7f4f9181564f4d7e773f10e6ad53a0f7ea5e
管梦秋 <634880846@qq.com>
2015-04-22 19:18:55 +0800

    球员排序

--- src/main/java/ui/playerui/PlayerPanel.java
+++ src/main/java/ui/playerui/PlayerPanel.java
@@ -288,7 +288,7 @@ public class PlayerPanel extends JPanel {
 	                return 0;  
 	            }  
 	        };  
-	        for (int col = 1; col < table.getColumnCount(); col++) {  
+	        for (int col = 2; col < table.getColumnCount(); col++) {  
 	            rowSorter.setComparator(col, numberComparator);  
 	        }  
 		jScrollPane = new JScrollPane(mytable);
@@ -835,6 +835,7 @@ public class PlayerPanel extends JPanel {
 		table.setDataVector(data, columnsName);
 	}
 
+//	private void update
 	private void setOnePlayerMessagePanel(){
 		playerMessagePanel.setLayout(new GridLayout(14,6,-1,-1));
 		playerMessagePanel.setBackground(FrameSize.backColor);
