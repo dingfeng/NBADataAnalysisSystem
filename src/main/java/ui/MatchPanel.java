a60a971f45acf7c3fe14c381e735555d72cd2ba3
管梦秋 <634880846@qq.com>
2015-04-22 15:54:09 +0800

    match的球员信息可以排序

--- src/main/java/ui/MatchPanel.java
+++ src/main/java/ui/MatchPanel.java
@@ -21,6 +21,8 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
 
 import po.MatchPlayerPO;
 import po.MatchTeamPO;
@@ -570,6 +572,7 @@ public class MatchPanel extends JPanel {
 		}
 		player1Table = new DefaultTableModel(data1, columnsName);
 		myPlayer1Table = new MyTable(player1Table);
+		myPlayer1Table.setRowSorter(new TableRowSorter<TableModel>(player1Table));
 		myPlayer1Table.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent e) {
 				if (e.getClickCount() == 2) {
@@ -617,6 +620,7 @@ public class MatchPanel extends JPanel {
 		}
 		player2Table = new DefaultTableModel(data2, columnsName);
 		myPlayer2Table = new MyTable(player2Table);
+		myPlayer2Table.setRowSorter(new TableRowSorter<TableModel>(player2Table));
 		myPlayer2Table.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent e) {
 				if (e.getClickCount() == 2) {
