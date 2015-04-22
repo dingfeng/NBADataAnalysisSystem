42f1d843ff8df5933cda219ece6005adfb7a417e
管梦秋 <634880846@qq.com>
2015-04-22 19:09:16 +0800

    .

--- src/main/java/ui/playerui/PlayerPanel.java
+++ src/main/java/ui/playerui/PlayerPanel.java
@@ -209,8 +209,8 @@ public class PlayerPanel extends JPanel {
 			// rowData.add(playerVO.getExp());
 			// rowData.add(playerVO.getSchool());
 			rowData.add(playerVO.getTeam());
-			rowData.add(playerVO.getMatchNo());
-			rowData.add(playerVO.getFirstServiceNo());
+			rowData.add(FrameSize.roundForNumber(playerVO.getMatchNo()));
+			rowData.add(FrameSize.roundForNumber(playerVO.getFirstServiceNo()));
 			rowData.add(FrameSize.roundForNumber(playerVO.getRebs()));
 			rowData.add(FrameSize.roundForNumber(playerVO.getAssistNo()));
 			rowData.add(FrameSize.roundForNumber(playerVO.getTime()));
